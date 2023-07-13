c9d7ee45dc2c1250862415e1b384593b22234750
Sravankumar1612 <sravankumartikkavarapu@gmail.com>
2023-07-13 14:37:00 +0530

    Intial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number > 0)
+        {
+            Console.WriteLine("The number is positive.");
+        }
+        else if (number < 0)
+        {
+            Console.WriteLine("The number is negative.");
+        }
+        else
+        {
+            Console.WriteLine("The number is zero.");
+        }
+
+        string evenodd = (number % 2 == 0) ? "even" : "odd";
+        Console.WriteLine("The number is " + evenodd + ".");
+        Console.ReadKey();
+    }
+}
